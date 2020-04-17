dbbca1dfab0ef7f45eafdb0316c3c38c2032094e
Mikhail Khromov <khromov05@gmail.com>
2020-04-18 02:50:38 +0300

    some fixes

--- khromoff/static_context.py
+++ khromoff/static_context.py
@@ -4,7 +4,7 @@
 def static_context(request):
     context = {'static': {
         # TODO: rename to https
-        'HOSTNAME': 'http://' + request.META['HTTP_HOST'],
+        'HOSTNAME': 'https://' + request.META['HTTP_HOST'],
         'urls': {'MY_VK': 'https://vk.com/id516131573',
                  'MY_TELEGRAM': 'https://t.me/Mikhail_Khromov',
                  'MY_GITHUB': 'https://github.com/mikhailkhromov'
